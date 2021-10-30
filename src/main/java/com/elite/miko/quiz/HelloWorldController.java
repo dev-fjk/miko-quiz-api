09a35687b2d23d2a155afe48128bdcb98b8c6f6c
dev-fjk <fjk.develop@gmail.com>
2021-10-30 22:41:45 +0900

    Swagger の導入

---
+++ src/main/java/com/elite/miko/quiz/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.elite.miko.quiz;
+
+import java.util.HashMap;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/miko/v1/")
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello/")
+    public ResponseEntity<?> get() {
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity<>("Hello World!", httpHeaders, HttpStatus.OK);
+    }
+}
