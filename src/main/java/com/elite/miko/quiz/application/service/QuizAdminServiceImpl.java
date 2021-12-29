3457a6ca42297ee5999ce01765bf1c6dacb29f3f
dev-fjk <fjk.develop@gmail.com>
2021-12-29 11:23:46 +0900

    管理者側のクイズ追加処理の実装

--- src/main/java/com/elite/miko/quiz/application/service/QuizAdminServiceImpl.java
+++ src/main/java/com/elite/miko/quiz/application/service/QuizAdminServiceImpl.java
@@ -1,13 +1,24 @@
 package com.elite.miko.quiz.application.service;
 
+import com.elite.miko.quiz.application.exception.RepositoryControlException;
+import com.elite.miko.quiz.domain.model.consts.QuizStatus;
 import com.elite.miko.quiz.domain.model.dto.QuizAddDto;
 import com.elite.miko.quiz.domain.model.dto.QuizUpdateDto;
+import com.elite.miko.quiz.domain.repository.AnswerRepository;
+import com.elite.miko.quiz.domain.repository.QuizRepository;
 import com.elite.miko.quiz.domain.service.QuizAdminService;
+import com.elite.miko.quiz.infrastructure.model.entity.Quiz;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class QuizAdminServiceImpl implements QuizAdminService {
+
+    private final QuizRepository quizRepository;
+    private final AnswerRepository answerRepository;
 
     @Override
     public Object fetchQuiz(int start, int count) {
@@ -19,9 +30,23 @@ public class QuizAdminServiceImpl implements QuizAdminService {
         return null;
     }
 
+    /**
+     * クイズの追加を行う
+     *
+     * @param quizAddDto : クイズ追加Dao
+     */
     @Override
+    @Transactional(rollbackFor = Throwable.class)
     public void insertQuiz(QuizAddDto quizAddDto) {
 
+        // 管理者側から追加する際は有効で登録
+        quizAddDto.setStatus(QuizStatus.ENABLED);
+
+        final Quiz insertedQuiz = quizRepository.insertQuiz(quizAddDto);
+        boolean isInsertedAnswer = answerRepository.insertAnswer(insertedQuiz.getQuizId(), quizAddDto.getAnswer());
+        if (!isInsertedAnswer) {
+            throw new RepositoryControlException("回答の登録に失敗しました");
+        }
     }
 
     @Override
